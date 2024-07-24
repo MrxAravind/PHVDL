69c189c501f86c18b6737a5d006bc18d7287c734
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-07-25 00:58:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ async def video(client, message):
         for video_url in video_urls:
             try:
                 downloaded_video_path = download_video(video_url, output_path=download_dir)
-                status = await status.edit_text("Video Has Been Downloaded..")
+                status = await status.edit_text("Video Has Been Downloaded..\nand Started To Upload")
                 exact_file_path = None
                 thumbnail_path = None
                 for root, dirs, files in os.walk(download_dir):
@@ -82,11 +82,11 @@ async def video(client, message):
                         elif file.endswith(('.jpg', '.png', '.webp')):
                             thumbnail_path = os.path.join(root, file)
                 if exact_file_path and thumbnail_path:
-                    status = await status.edit_text("Video Starting to Upload..")
+                    #status = await status.edit_text("Video Starting to Upload..")
                     await upload_video(app, chat_id, exact_file_path, thumbnail_path)
                 else:
                     logging.error(f"Downloaded video or thumbnail file not found in '{download_dir}' directory.")
-                    status = await status.edit_text(f"Downloaded video or thumbnail file not found in '{download_dir}' directory.")
+                    #status = await status.edit_text(f"Downloaded video or thumbnail file not found in '{download_dir}' directory.")
                     
             except Exception as e:
                 status = await status.edit_text(f"Error Occured:{e}")
