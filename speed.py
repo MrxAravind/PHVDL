8c3ca160bb58e10963ef5a2701d3645f294f6626
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-07-25 11:39:57 +0530

    Update speed.py

--- speed.py
+++ speed.py
@@ -1,42 +1,48 @@
 import speedtest
 
-
 def get_readable_file_size(size):
     return f"{size / (1024 ** 2):.2f} MB"
 
-
 def get_speedtest_stats():
-    test = speedtest.Speedtest()
-    test.get_best_server()
-    test.download()
-    test.upload()
-    test.results.share()
-    result = test.results.dict()
-    path = result['share']
+    try:
+        test = speedtest.Speedtest()
+        test.get_best_server()
+        test.download()
+        test.upload()
+        test.results.share()
+        result = test.results.dict()
+        path = result['share']
 
-    stats = f'''
-➲ <b><i>SPEEDTEST INFO</i></b>
-┠ <b>Upload:</b> <code>{get_readable_file_size(result['upload'] / 8)}/s</code>
-┠ <b>Download:</b> <code>{get_readable_file_size(result['download'] / 8)}/s</code>
-┠ <b>Ping:</b> <code>{result['ping']} ms</code>
-┠ <b>Time:</b> <code>{result['timestamp']}</code>
-┠ <b>Data Sent:</b> <code>{get_readable_file_size(int(result['bytes_sent']))}</code>
-┖ <b>Data Received:</b> <code>{get_readable_file_size(int(result['bytes_received']))}</code>
+        stats = f'''
+        ➲ <b><i>SPEEDTEST INFO</i></b>
+        ┠ <b>Upload:</b> <code>{get_readable_file_size(result['upload'] / 8)}/s</code>
+        ┠ <b>Download:</b> <code>{get_readable_file_size(result['download'] / 8)}/s</code>
+        ┠ <b>Ping:</b> <code>{result['ping']} ms</code>
+        ┠ <b>Time:</b> <code>{result['timestamp']}</code>
+        ┠ <b>Data Sent:</b> <code>{get_readable_file_size(int(result['bytes_sent']))}</code>
+        ┖ <b>Data Received:</b> <code>{get_readable_file_size(int(result['bytes_received']))}</code>
 
-➲ <b><i>SPEEDTEST SERVER</i></b>
-┠ <b>Name:</b> <code>{result['server']['name']}</code>
-┠ <b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
-┠ <b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
-┠ <b>Latency:</b> <code>{result['server']['latency']}</code>
-┠ <b>Latitude:</b> <code>{result['server']['lat']}</code>
-┖ <b>Longitude:</b> <code>{result['server']['lon']}</code>
+        ➲ <b><i>SPEEDTEST SERVER</i></b>
+        ┠ <b>Name:</b> <code>{result['server']['name']}</code>
+        ┠ <b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
+        ┠ <b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
+        ┠ <b>Latency:</b> <code>{result['server']['latency']}</code>
+        ┠ <b>Latitude:</b> <code>{result['server']['lat']}</code>
+        ┖ <b>Longitude:</b> <code>{result['server']['lon']}</code>
 
-➲ <b><i>CLIENT DETAILS</i></b>
-┠ <b>IP Address:</b> <code>{result['client']['ip']}</code>
-┠ <b>Latitude:</b> <code>{result['client']['lat']}</code>
-┠ <b>Longitude:</b> <code>{result['client']['lon']}</code>
-┠ <b>Country:</b> <code>{result['client']['country']}</code>
-┠ <b>ISP:</b> <code>{result['client']['isp']}</code>
-┖ <b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
-'''
-    return path,stats
+        ➲ <b><i>CLIENT DETAILS</i></b>
+        ┠ <b>IP Address:</b> <code>{result['client']['ip']}</code>
+        ┠ <b>Latitude:</b> <code>{result['client']['lat']}</code>
+        ┠ <b>Longitude:</b> <code>{result['client']['lon']}</code>
+        ┠ <b>Country:</b> <code>{result['client']['country']}</code>
+        ┠ <b>ISP:</b> <code>{result['client']['isp']}</code>
+        ┖ <b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
+        '''
+        return path, stats
+    
+    except speedtest.SpeedtestException as e:
+        return None, f"Speedtest failed: {str(e)}"
+    except KeyError as e:
+        return None, f"KeyError: {str(e)} occurred while accessing results."
+    except Exception as e:
+        return None, f"An error occurred: {str(e)}"
