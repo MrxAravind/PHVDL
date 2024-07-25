ae9c6820cd262610e4b27b7e5c44146917b2f280
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-07-25 10:42:34 +0530

    Update speed.py

--- speed.py
+++ speed.py
@@ -1,32 +1,12 @@
 import speedtest
-from speedtest import ConfigRetrievalError
-import aiohttp
-import asyncio
-import logging
 
-LOGGER = logging.getLogger(__name__)
 
-async def get_readable_file_size(size):
+def get_readable_file_size(size):
     return f"{size / (1024 ** 2):.2f} MB"
 
-async def send_message(message, content, photo=None):
-    print(content)
-    return content
 
-async def edit_message(message, new_content):
-    print(new_content)
-
-async def delete_message(message):
-    pass
-
-async def speedtest(app, message):
-    speed = await app.send_message(message.chat.id, "<i>Initiating Speedtest...</i>")
-    try:
-        test = speedtest.Speedtest()
-    except ConfigRetrievalError:
-        await speed.edit_text("<b>ERROR:</b> <i>Can't connect to Server at the Moment, Try Again Later !</i>")
-        return
-
+def speedtest():
+    test = speedtest.Speedtest()
     test.get_best_server()
     test.download()
     test.upload()
@@ -34,14 +14,14 @@ async def speedtest(app, message):
     result = test.results.dict()
     path = result['share']
 
-    string_speed = f'''
+    stats = f'''
 ➲ <b><i>SPEEDTEST INFO</i></b>
-┠ <b>Upload:</b> <code>{await get_readable_file_size(result['upload'] / 8)}/s</code>
-┠ <b>Download:</b> <code>{await get_readable_file_size(result['download'] / 8)}/s</code>
+┠ <b>Upload:</b> <code>{get_readable_file_size(result['upload'] / 8)}/s</code>
+┠ <b>Download:</b> <code>{get_readable_file_size(result['download'] / 8)}/s</code>
 ┠ <b>Ping:</b> <code>{result['ping']} ms</code>
 ┠ <b>Time:</b> <code>{result['timestamp']}</code>
-┠ <b>Data Sent:</b> <code>{await get_readable_file_size(int(result['bytes_sent']))}</code>
-┖ <b>Data Received:</b> <code>{await get_readable_file_size(int(result['bytes_received']))}</code>
+┠ <b>Data Sent:</b> <code>{get_readable_file_size(int(result['bytes_sent']))}</code>
+┖ <b>Data Received:</b> <code>{get_readable_file_size(int(result['bytes_received']))}</code>
 
 ➲ <b><i>SPEEDTEST SERVER</i></b>
 ┠ <b>Name:</b> <code>{result['server']['name']}</code>
@@ -59,9 +39,4 @@ async def speedtest(app, message):
 ┠ <b>ISP:</b> <code>{result['client']['isp']}</code>
 ┖ <b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
 '''
-    try:
-        pho = await app.send_photo(message.chat.id, photo=path,caption= string_speed)
-        await delete_message(speed)
-    except Exception as e:
-        LOGGER.error(str(e))
-        await message.edit_text(speed, string_speed)
+    return img,stats
