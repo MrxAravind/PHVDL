634a360dc4bd09b0e26199a69ed61376a7f04cf5
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-07-26 12:59:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.environ.get('TELEGRAM_API', ''))
-API_HASH = os.environ.get('TELEGRAM_HASH', '')
+API_ID = int(os.environ.get('API_ID', ''))
+API_HASH = os.environ.get('API_HASH', '')
 DUMP_ID = int(os.environ.get('DUMP_CHAT_ID', ''))
 DATABASE = os.getenv("DATABASE")
 LOG_ID = int(os.environ.get('LOG_CHAT_ID', ''))
