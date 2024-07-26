03c3bf702e1bad4d43b16931dd43020f1d65ba97
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-07-26 14:41:33 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,5 +1,4 @@
 import asyncio
-import telegram
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -16,6 +15,15 @@ def fetch_video_links():
     return [div.find('a', class_='thumbnailTitle')['href'].replace("https://cf-proxy.mrspidyxd.workers.dev", url).split("&")[0] for div in soup.find_all('div', class_='vidTitleWrapper') if div.find('a', class_='thumbnailTitle')]
 
 
+def send_message(text,chat_id):
+        url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+        payload = {
+            'chat_id': chat_id,
+            'text': text
+        }
+        response = requests.post(url, data=payload)
+        print(response.json())
+
 
 
 def main():
@@ -24,11 +32,10 @@ def main():
     urls = fetch_video_links()
     length = len(urls)
     urls = [" ".join(urls[0:30])," ".join(urls[30:-1])]
-    bot = telegram.Bot(BOT_TOKEN)
     for url in urls:
-           bot.send_message(text=url,chat_id=DUMP_ID)
+           send_message(text=url,chat_id=DUMP_ID)
+           send_message(text=f"{len(url.split())} out of {length} Videos has Been Sent",chat_id=LOG_ID)
            time.sleep(600)
-           bot.send_message(text=f"{len(url.split())} out of {length} Videos has Been Sent",chat_id=LOG_ID)
     time.sleep(3600)
 
 if __name__ == '__main__':
