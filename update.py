ca7b532bee349f662ebbcd25796b5d04be33b610
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-07-27 14:13:45 +0530

    Update update.py

--- update.py
+++ update.py
@@ -21,6 +21,7 @@ def fetch_video_links():
 def search_video_links(query):
     base_url = "https://cf-proxy.mrspidyxd.workers.dev/?host="
     search_url = "https://www.pornhub.com/video/search?search="
+    url = "https://www.pornhub.com"
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
     response = requests.get(base_url + search_url + query, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
